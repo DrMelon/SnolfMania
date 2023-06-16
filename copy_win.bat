0ac1bb43b4b8b1ef8f1c843fdba13fbf66222774
DrMelon <drmelon.mail@gmail.com>
2023-06-16 15:36:48 +0100

    Darned windows!

--- copy_win.bat
+++ copy_win.bat
@@ -1,3 +1,3 @@
-copy mods/SnolfMania/Release/SnolfMania.dll mods/SnolfMania/SnolfMania.dll
-del mods/SnolfMania/Release/SnolfMania.dll
-rmdir mods/SnolfMania/Release
+copy mods\SnolfMania\Release\SnolfMania.dll mods\SnolfMania\SnolfMania.dll
+del mods\SnolfMania\Release\SnolfMania.dll
+rmdir mods\SnolfMania\Release
